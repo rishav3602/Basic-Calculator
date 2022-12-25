93d30cff1b94dd0e55d2865a3ac75cbd11ca7b48
Rishav Kumar <113835701+rishav3602@users.noreply.github.com>
2022-12-25 16:14:48 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,40 @@
+class Calculator:
+    def add (x,y):
+        return x + y
+
+    def sub (x,y):
+        return x - y
+
+    def mul (x,y):
+        return x * y
+
+    def div (x,y):
+        return x / y
+
+    while True:
+
+        num1 = float(input("Enter your first number : "))
+        num2 = float(input("Enter your second numbe : "))
+        opr = input("Select the operator : add(a) , subtract(s) , multiply(m) , divide(d) : ")
+
+        if opr == "a":
+            print(f"{num1} + {num2} = {add(num1,num2)}")
+        elif opr == "s":
+            print(f"{num1} - {num2} = {sub(num1,num2)}")
+        elif opr == "m":
+            print(f"{num1} X {num2} = {mul(num1,num2)}")
+        elif opr == "d":
+            print(f"{num1} / {num2} = {div(num1,num2)}")
+        else :
+            print("invalid input")
+        
+        next_calculation = input("Select YES(y) or NO(n) to do next calculation: ")
+        if next_calculation == "n":
+            break
+        elif next_calculation == "y":
+            continue
+        else:
+            print("invalid input")
+            break
+
+num = Calculator()
